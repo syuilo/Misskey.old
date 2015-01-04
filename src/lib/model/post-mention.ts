b4bddba3f9425c1c4d4989223a3fa66525937b5a
syuilo <syuilotan@yahoo.co.jp>
2015-01-04 21:39:55 +0900

    Add files

---
+++ src/lib/model/post-mention.ts
@@ -0,0 +1,46 @@
+/// <reference path="../../../typings/bundle.d.ts" />
+
+import db = require('./db');
+export = PostMention;
+
+class PostMention {
+	id: number;
+	postId: number;
+	userId: number;
+
+	public constructor(postMention: any) {
+		this.id = postMention.id;
+		this.postId = postMention.post_id;
+		this.userId = postMention.user_id;
+	}
+
+	public static create(postId: number, userId: number, callback: (postMention: PostMention) => void): void {
+		db.query('insert into post_mentions (post_id, user_id) values (?, ?)',
+			[postId, userId],
+			(err, postMentions) => callback(new PostMention(postMentions[0])));
+	}
+
+	public static findByUserId(userId: number, limit: number, offset: number, callback: (postMentions: PostMention[]) => void): void {
+		var q, p;
+		if (limit === null) {
+			q = "select * from post_mentions where user_id = ? order by id desc";
+			p = [userId];
+		} else {
+			q = "select * from post_mentions where user_id = ? order by id desc limit ?, ?";
+			p = [userId, offset, limit];
+		}
+		db.query(q, p, (err, postMentions: any[]) => callback(postMentions.map((postMention) => new PostMention(postMention))));
+	}
+
+	public static findByPostId(postId: number, limit: number, offset: number, callback: (postMentions: PostMention[]) => void): void {
+		var q, p;
+		if (limit === null) {
+			q = "select * from post_mentions where post_id = ? order by id desc";
+			p = [postId];
+		} else {
+			q = "select * from post_mentions where post_id = ? order by id desc limit ?, ?";
+			p = [postId, offset, limit];
+		}
+		db.query(q, p, (err, postMentions: any[]) => callback(postMentions.map((postMention) => new PostMention(postMention))));
+	}
+}
---
+++ src/lib/model/sauth-request-token.ts
@@ -0,0 +1,58 @@
+/// <reference path="../../../typings/bundle.d.ts" />
+
+import db = require('./db');
+export = SAuthRequestToken;
+
+import crypto = require('crypto');
+
+var createHash = (() => {
+	var sha1sum = crypto.createHash('sha256');
+	return (text: string) => {
+		sha1sum.update(text);
+		return sha1sum.digest('hex');
+	};
+})();
+
+class SAuthRequestToken {
+	appId: number;
+	id: number;
+	isInvalid: boolean;
+	token: string;
+
+	public constructor(sauthRequestToken: any) {
+		this.appId = sauthRequestToken.app_id;
+		this.id = sauthRequestToken.id;
+		this.isInvalid = sauthRequestToken.invalid;
+		this.token = sauthRequestToken.token;
+	}
+
+	public static generateToken(appId: number): string {
+		return createHash(appId + (+new Date()).toString());
+	}
+
+	public static create(appId: number, callback: (sauthRequestToken: SAuthRequestToken) => void): void {
+		var token = SAuthRequestToken.generateToken(appId);
+		db.query('insert into sauth_request_tokens (app_id, token) values (?, ?)',
+			[appId, token],
+			(err, sauthRequestTokens) => callback(new SAuthRequestToken(sauthRequestTokens[0])));
+	}
+
+	public static find(requestToken: string, callback: (sauthRequestToken: SAuthRequestToken) => void): void {
+		db.query("select * from sauth_request_tokens where token = ?",
+			[requestToken],
+			(err, sauthRequestTokens) => callback(new SAuthRequestToken(sauthRequestTokens[0])));
+	}
+
+    public update(callback?: () => void): void {
+		db.query('update sauth_request_tokens set invalid = ? where token = ?',
+			[this.isInvalid, this.token],
+			callback);
+    }
+
+    public destroy(callback?: () => void): void {
+        db.query("delete from sauth_request_tokens where token=?",
+            [this.token],
+            callback);
+    }
+    
+}
