a286db608f648beb32cbe0c74df005b67c0d8875
syuilo <syuilotan@yahoo.co.jp>
2015-01-09 07:46:28 +0900

    Implement router

---
+++ src/lib/web/router.ts
@@ -0,0 +1,80 @@
+/// <reference path="../../../typings/bundle.d.ts" />
+
+import express = require('express');
+import User = require('../model/user');
+
+export = router;
+
+var extend = (destination, source): Object => {
+	for (var k in source) {
+		if (source.hasOwnProperty(k)) {
+			destination[k] = source[k];
+		}
+	}
+	return destination;
+}
+
+function router(app: express.Express): void {
+	
+	app.all('*', (req: express.Request, res: express.Response, next) => {
+		app.disable('x-powered-by');
+		res.set({
+			'Access-Control-Allow-Origin': 'http://llli.co',
+			'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+			'Access-Control-Allow-Credentials': true,
+			'X-Frame-Options': 'DENY'
+		});
+
+		req.login = (req.session != null && req.session.userId != null);
+
+		req.data = {};
+		req.data.siteUrl = config.siteUrl;
+		req.data.apiUrl = config.apiUrl;
+		req.data.login = login;
+
+		req.pretty = '  ';
+		res.display = display;
+
+		if (req.login) {
+			var userId = req.session.userId;
+			User.find(userId, (user: User) => {
+				req.data.me = user;
+				req.me = user;
+				next();
+			});
+		} else {
+			req.data.me = null;
+			req.me = null;
+			next();
+		}
+	});
+
+	app.param('userSn', (req: express.Request, res: express.Response, next, sn: string) => {
+		User.findByScreenName(sn, (user: User) => {
+			if (user != null) {
+				req.rootUser = user;
+				next();
+			} else {
+				display(req, res, 'user-notFound', {
+				});
+			}
+		});
+	});
+
+	app.get('/', (req: express.Request, res: express.Response) => {
+		display(req, res, 'index', { });
+	});
+
+	app.get('/logout', function (req: express.Request, res: express.Response) {
+		req.session.destroy(function (err) {
+			res.redirect('/');
+		})
+	});
+
+	app.get('/:userSn', require('./models/user'));
+
+	function display(req: express.Request, res: express.Response, name: string, renderData: any) {
+		/* Mixin */
+		res.render(name, extend(req.data, renderData));
+	}
+};
