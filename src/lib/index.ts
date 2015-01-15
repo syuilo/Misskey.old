5edfbbf5fc8b2d62e8ef01800ad28e0bca806735
syuilo <syuilotan@yahoo.co.jp>
2015-01-15 14:08:56 +0900

    Remove argument

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../typings/bundle.d.ts" />
 
 module.exports = (config: any) => {
-	var db = require('./db')(config);
+	var db = require('./db');
 	require('./web')(config, db);
 	require('./api')(config, db);
 };
