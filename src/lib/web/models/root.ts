f0263fc3c32d47be5bea962e288f578e361060ee
syuilo <syuilotan@yahoo.co.jp>
2015-01-16 13:12:37 +0900

    Move generate timeline method

--- src/lib/web/models/root.ts
+++ src/lib/web/models/root.ts
@@ -12,28 +12,9 @@ export = render;
 var render = (req: any, res: any): void => {
 	if (req.login) {
 		Post.getTimeline(req.me.id, 30, null, null, (posts: Post[]) => {
-			async.map(posts, (post: any, next: any) => {
-				post.isReply = post.inReplyToPostId != null;
-				User.find(post.userId, (user: User) => {
-					post.user = user;
-					Application.find(post.appId, (app: Application) => {
-						post.app = app;
-						if (post.isReply) {
-							Post.find(post.inReplyToPostId, (replyPost: any) => {
-								post.reply = replyPost;
-								User.find(post.reply.userId, (replyUser: User) => {
-									post.reply.user = replyUser;
-									next(null, post);
-								});
-							});
-						} else {
-							next(null, post);
-						}
-					});
-				})
-			}, (err, results) => {
+			Post.generateTimeline(posts, (timeline: Post[]) => {
 				res.display(req, res, 'home', {
-					timeline: results
+					timeline: timeline
 				});
 			});
 		});
