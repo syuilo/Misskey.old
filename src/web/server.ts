1b6cecf88f03a7b67188e983e04f1bf09b9f614e
syuilo <syuilotan@yahoo.co.jp>
2015-02-06 03:08:53 +0900

    Update directory structure (resources -> statics)

--- src/web/server.ts
+++ src/web/server.ts
@@ -14,7 +14,7 @@ var RedisStore: any = require('connect-redis')(session);
 var webServer = express();
 webServer.set('view engine', 'jade');
 webServer.set('views', __dirname + '/views');
-webServer.use(express.static(__dirname + '/resources'));
+webServer.use(express.static(__dirname + '/statics'));
 webServer.use(bodyParser.urlencoded({ extended: true }));
 webServer.use(cookieParser(config.cookie_pass));
 
