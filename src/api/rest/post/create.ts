372e02fb387a4de1c4d937d8aed29c0e467d630f
syuilo <syuilotan@yahoo.co.jp>
2015-02-07 04:01:44 +0900

    Save image

--- src/api/rest/post/create.ts
+++ src/api/rest/post/create.ts
@@ -1,5 +1,7 @@
 /// <reference path="../../../../typings/bundle.d.ts" />
 
+import fs = require('fs');
+import jpeg = require('jpeg-js');
 import APIResponse = require('../../api-response');
 import AccessToken = require('../../../models/access-token');
 import Application = require('../../../models/application');
@@ -15,14 +17,17 @@ var postCreate = (req: any, res: APIResponse) => {
 	authorize(req, res, (user: User, app: Application) => {
 		var text = req.body.text != null ? req.body.text : '';
 		var irtpi = req.body.in_reply_to_post_id != null ? req.body.in_reply_to_post_id : null;
-		var image = req.body.image != null ? req.body.image : null;
+		var image = null;
 		var isImageAttached = false;
-		if (image != null) {
-			console.log(image);
+		if (req.files != null) {
+			console.log(req.files);
 			isImageAttached = true;
+			var path = req.files.path;
+			image = jpeg.encode(fs.readFileSync(path), 50);
+			fs.unlink(path);
 		}
 
-		Post.create(app.id, irtpi, null, isImageAttached, text, user.id, (post: Post) => {
+		Post.create(app.id, irtpi, image, isImageAttached, text, user.id, (post: Post) => {
 			var streamObj: any = {};
 			streamObj.type = 'post';
 			streamObj.value = post;
