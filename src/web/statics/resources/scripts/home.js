13c9b71f792fb478cde6babcf95522a1b2b76582
syuilo <syuilotan@yahoo.co.jp>
2015-02-09 07:57:19 +0900

    Add animation

--- src/web/statics/resources/scripts/home.js
+++ src/web/statics/resources/scripts/home.js
@@ -1,46 +1,54 @@
-$(function() {
-	socket = io.connect('https://api.misskey.xyz:1207/streaming/home', { port: 1207 });
+$(function()
+{
+    socket = io.connect('https://api.misskey.xyz:1207/streaming/home', { port: 1207 });
 
-	socket.on('connected', function() {
-		console.log('Connected');
-	});
+    socket.on('connected', function()
+    {
+        console.log('Connected');
+    });
 
-	socket.on('disconnect', function(client) {
-	});
+    socket.on('disconnect', function(client)
+    {
+    });
 
-	socket.on('post', function(content) {
-	    console.log(content);
-	    $('#timeline > .timeline > .posts').prepend(generatePost(content, conf));
-	});
+    socket.on('post', function(content)
+    {
+        console.log(content);
+        var $post = generatePost(content, conf).hide();
+        $post.prependTo($('#timeline > .timeline > .posts')).show(200);
+    });
 
 
-	$('#postForm').submit(function(event) {
-		event.preventDefault();
-		var $form = $(this);
-		var $submitButton = $form.find('[type=submit]');
+    $('#postForm').submit(function(event)
+    {
+        event.preventDefault();
+        var $form = $(this);
+        var $submitButton = $form.find('[type=submit]');
 
-		$submitButton.attr('disabled', true);
-		$submitButton.text('Updating...');
+        $submitButton.attr('disabled', true);
+        $submitButton.text('Updating...');
 
-		$.ajax($form.attr('action'), {
-			type: $form.attr('method'),
-			processData: false,
-			contentType: false,
-			data: new FormData($form[0]),
-			dataType: 'json',
-			xhrFields: {
-				withCredentials: true
-			}
-		}).done(function(data) {
-			$form[0].reset();
-			$submitButton.attr('disabled', false);
-			$submitButton.text('Update');
-		}).fail(function(data) {
-			$form[0].reset();
-			/*alert('error');*/
-			$submitButton.attr('disabled', false);
-			$submitButton.text('Update');
-		});
-	});
+        $.ajax($form.attr('action'), {
+            type: $form.attr('method'),
+            processData: false,
+            contentType: false,
+            data: new FormData($form[0]),
+            dataType: 'json',
+            xhrFields: {
+                withCredentials: true
+            }
+        }).done(function(data)
+        {
+            $form[0].reset();
+            $submitButton.attr('disabled', false);
+            $submitButton.text('Update');
+        }).fail(function(data)
+        {
+            $form[0].reset();
+            /*alert('error');*/
+            $submitButton.attr('disabled', false);
+            $submitButton.text('Update');
+        });
+    });
 });
 
