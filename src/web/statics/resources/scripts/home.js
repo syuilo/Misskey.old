85c286eeb4cf4123ac207865a4af2455b8d6e110
syuilo <syuilotan@yahoo.co.jp>
2015-02-08 21:30:39 +0900

    Use Ajax

--- src/web/statics/resources/scripts/home.js
+++ src/web/statics/resources/scripts/home.js
@@ -1,14 +1,45 @@
 $(function() {
-	socket = io.connect("https://api.misskey.xyz:1207/streaming/home", { port: 1207 });
+	socket = io.connect('https://api.misskey.xyz:1207/streaming/home', { port: 1207 });
 
-	socket.on("connected", function() {
+	socket.on('connected', function() {
 		console.log('Connected');
 	});
 
-	socket.on("disconnect", function(client) {
+	socket.on('disconnect', function(client) {
 	});
 
-	socket.on("post", function(content) {
+	socket.on('post', function(content) {
 		console.log(content);
 	});
+
+
+	$('#postForm').submit(function(event) {
+		event.preventDefault();
+		var $form = $(this);
+		var $submitButton = $form.find('[type=submit]');
+
+		$submitButton.attr('disabled', true);
+		$submitButton.text('‘—M’†...');
+
+		$.ajax($form.attr('action'), {
+			type: $form.attr('method'),
+			processData: false,
+			contentType: false,
+			data: new FormData($form[0]),
+			dataType: 'json',
+			xhrFields: {
+				withCredentials: true
+			}
+		}).done(function(data) {
+			$form[0].reset();
+			$submitButton.attr('disabled', false);
+			$submitButton.text('‘—M');
+		}).fail(function(data) {
+			$form[0].reset();
+			/*alert('error');*/
+			$submitButton.attr('disabled', false);
+			$submitButton.text('‘—M');
+		});
+	});
 });
+
