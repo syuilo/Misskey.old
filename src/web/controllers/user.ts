cc37b474b575b0ce0124ee2e9d65d1f8d8ce68f8
syuilo <syuilotan@yahoo.co.jp>
2015-02-09 23:44:08 +0900

    Set tags

--- src/web/controllers/user.ts
+++ src/web/controllers/user.ts
@@ -9,6 +9,7 @@ export = render;
 var render = (req: any, res: any): void => {
 	res.display(req, res, 'user', {
 		user: req.rootUser,
+		tags: req.rootUser.split(','),
 		url: conf.publicConfig.url
 	});
 };
