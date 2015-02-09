aa56907b7483de8de1cc50f87d59e942e45a6f31
syuilo <syuilotan@yahoo.co.jp>
2015-02-09 23:25:49 +0900

    Add controller

---
+++ src/web/controllers/user.ts
@@ -0,0 +1,13 @@
+/// <reference path="../../../typings/bundle.d.ts" />
+
+import User = require('../../models/user');
+import Post = require('../../models/post');
+import conf = require('../../config');
+
+export = render;
+
+var render = (req: any, res: any): void => {
+	res.display(req, res, 'user', {
+		user: req.rootUser
+	});
+};
