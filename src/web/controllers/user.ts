d597c77f9357cd4a11aafadc189ab283b8465585
syuilo <syuilotan@yahoo.co.jp>
2015-02-10 02:48:21 +0900

    Display user timeline

--- src/web/controllers/user.ts
+++ src/web/controllers/user.ts
@@ -2,14 +2,20 @@
 
 import User = require('../../models/user');
 import Post = require('../../models/post');
+import Timeline = require('../utils/timeline');
 import conf = require('../../config');
 
 export = render;
 
 var render = (req: any, res: any): void => {
-	res.display(req, res, 'user', {
-		user: req.rootUser,
-		tags: req.rootUser.tag.split(','),
-		url: conf.publicConfig.url
+	Post.findByUserId(req.rootUser.id, 30, null, null,(posts: Post[]) => {
+		Timeline.generateHtml(posts,(timelineHtml: string) => {
+			res.display(req, res, 'user', {
+				user: req.rootUser,
+				tags: req.rootUser.tag.split(','),
+				url: conf.publicConfig.url,
+				timelineHtml: timelineHtml
+			});
+		});
 	});
 };
