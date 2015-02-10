4790139c5d695fd7f243e5e8edfac16731b3f2c8
syuilo <syuilotan@yahoo.co.jp>
2015-02-11 06:35:37 +0900

    Fix typo

--- src/web/resources/scripts/home.js
+++ src/web/resources/scripts/home.js
@@ -16,7 +16,7 @@ $(function() {
 
 	socket.on('post', function(post) {
 		console.log('post', post);
-		var $post = TIMELINE.generatePostElement(content, conf).hide()
+		var $post = TIMELINE.generatePostElement(post, conf).hide()
 		TIMELINE.setEventPost($post);
 		$post.prependTo($('#timeline .timeline > .posts')).show(200);
 	});
