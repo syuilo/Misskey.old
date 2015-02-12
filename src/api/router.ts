b61b1a72cb1a32269fe11b202dba7e3a35a550d4
syuilo <syuilotan@yahoo.co.jp>
2015-02-12 14:25:12 +0900

    Implement users/show

--- src/api/router.ts
+++ src/api/router.ts
@@ -9,6 +9,7 @@ function router(app: express.Express): void {
 	app.put('/account/update_icon', require('./rest/account/update_icon'));
 	app.put('/account/update_header', require('./rest/account/update_header'));
 	app.put('/account/update_wallpaper', require('./rest/account/update_wallpaper'));
+	app.get('/users/show', require('./rest/users/show'));
 	app.post('/users/follow', require('./rest/users/follow'));
 	app.delete('/users/unfollow', require('./rest/users/unfollow'));
 	app.post('/post/create', require('./rest/post/create'));
