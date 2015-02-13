ee11e64397d1ad5add7e0eeaaaa6406152ba3106
syuilo <syuilotan@yahoo.co.jp>
2015-02-14 05:57:07 +0900

    Refactoring

--- src/api/rest/users/follow.ts
+++ src/api/rest/users/follow.ts
@@ -3,7 +3,6 @@
 import fs = require('fs');
 import APIResponse = require('../../api-response');
 import Streamer = require('../../../utils/streaming');
-import AccessToken = require('../../../models/access-token');
 import Application = require('../../../models/application');
 import User = require('../../../models/user');
 import UserFollowing = require('../../../models/user-following');
@@ -12,31 +11,34 @@ var authorize = require('../../auth');
 
 var usersFollow = (req: any, res: APIResponse) => {
 	authorize(req, res,(user: User, app: Application) => {
-		if (req.body.user_id != null) {
-			var userId = req.body.user_id;
-			UserFollowing.isFollowing(user.id, userId,(isFollowing: boolean) => {
-				if (!isFollowing) {
-					User.find(userId,(targetUser: User) => {
-						if (targetUser != null) {
-							UserFollowing.create(targetUser.id, user.id,(following: UserFollowing) => {
-								var streamObj: any = {};
-								streamObj.type = 'followedMe';
-								streamObj.value = user.filt();
-								Streamer.publish('userStream:' + targetUser.id, JSON.stringify(streamObj));
-
-								res.apiRender(targetUser.filt());
-							});
-						} else {
-							res.apiError(404, 'User not found...');
-						}
-					});
-				} else {
-					res.apiError(400, 'This user is already following :)');
-				}
-			});
-		} else {
+		if (req.body.user_id == null) {
 			res.apiError(400, 'user_id parameter is required :(');
+			return;
 		}
+		var userId = req.body.user_id;
+
+		UserFollowing.isFollowing(user.id, userId,(isFollowing: boolean) => {
+			if (isFollowing) {
+				res.apiError(400, 'This user is already following :)');
+				return;
+			}
+
+			User.find(userId,(targetUser: User) => {
+				if (targetUser == null) {
+					res.apiError(404, 'User not found...');
+					return;
+				}
+
+				UserFollowing.create(targetUser.id, user.id,(following: UserFollowing) => {
+					var streamObj: any = {};
+					streamObj.type = 'followedMe';
+					streamObj.value = user.filt();
+					Streamer.publish('userStream:' + targetUser.id, JSON.stringify(streamObj));
+
+					res.apiRender(targetUser.filt());
+				});
+			});
+		});
 	});
 }
 
