b1bdaa46406546e99bae8f47b25bad09dbce96dd
syuilo <syuilotan@yahoo.co.jp>
2015-02-13 13:43:32 +0900

    Refactoring

--- src/api/authorize-get.ts
+++ src/api/authorize-get.ts
@@ -7,38 +7,40 @@ import User = require('../models/user');
 
 var AuthorizeGet = (req: any, res: any) => {
 	var login = (req.session != null && req.session.userId != null);
-	if (req.query.request_token != null) {
-		var requestToken = req.query.request_token;
-		SauthRequestToken.find(requestToken,(requestTokenInstance: SauthRequestToken) => {
-			if (requestTokenInstance != null) {
-				if (!requestTokenInstance.isInvalid) {
-					Application.find(requestTokenInstance.appId,(app: Application) => {
-						if (login) {
-							User.find(req.session.userId,(user: User) => {
-								res.render('../web/views/authorize-confirm', {
-									login: true,
-									me: user,
-									app: app
-								});
-							});
-						} else {
-							res.render('../web/views/authorize-confirm', {
-								login: false,
-								app: app,
-								loginFailed: false
-							});
-						}
+
+	if (req.query.request_token == null) {
+		res.apiError(400, 'consumer_key parameter is required :(');
+		return;
+	}
+	var requestToken = req.query.request_token;
+
+	SauthRequestToken.find(requestToken,(requestTokenInstance: SauthRequestToken) => {
+		if (requestTokenInstance == null) {
+			res.render('../web/views/authorize-invalidToken', {});
+			return;
+		}
+		if (requestTokenInstance.isInvalid) {
+			res.render('../web/views/authorize-invalidToken', {});
+			return;
+		}
+		Application.find(requestTokenInstance.appId,(app: Application) => {
+			if (login) {
+				User.find(req.session.userId,(user: User) => {
+					res.render('../web/views/authorize-confirm', {
+						login: true,
+						me: user,
+						app: app
 					});
-				} else {
-					res.render('../web/views/authorize-invalidToken', {});
-				}
+				});
 			} else {
-				res.render('../web/views/authorize-invalidToken', {});
+				res.render('../web/views/authorize-confirm', {
+					login: false,
+					app: app,
+					loginFailed: false
+				});
 			}
 		});
-	} else {
-		res.apiError(400, 'consumer_key parameter is required :(');
-	}
+	});
 }
 
 module.exports = AuthorizeGet;
