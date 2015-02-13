7910ada2ebc5e3afd4af6293a9927ac687488f60
syuilo <syuilotan@yahoo.co.jp>
2015-02-14 06:00:20 +0900

    Refactoring

--- src/api/rest/account/show.ts
+++ src/api/rest/account/show.ts
@@ -1,9 +1,7 @@
 /// <reference path="../../../../typings/bundle.d.ts" />
 
-import fs = require('fs');
 import APIResponse = require('../../api-response');
 import Streamer = require('../../../utils/streaming');
-import AccessToken = require('../../../models/access-token');
 import Application = require('../../../models/application');
 import User = require('../../../models/user');
 
