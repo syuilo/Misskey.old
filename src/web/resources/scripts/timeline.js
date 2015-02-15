757ae6d1afeecfa9bc6b8589db69cfdab38c0465
syuilo <syuilotan@yahoo.co.jp>
2015-02-15 10:43:41 +0900

    Update design

--- src/web/resources/scripts/timeline.js
+++ src/web/resources/scripts/timeline.js
@@ -145,9 +145,9 @@ TIMELINE.generatePostElement = function(post) {
 }
 
 TIMELINE.setEventPost = function($post) {
-	$post.find('article > a').click(function() {
-		var $content = $("<iframe>").attr({ src: $post.find('article > a').attr('href'), seamless: true });
-		openWindow($content, "トーク: " + $post.find('article > header > h2 > a').text(), 300, 450);
+	$post.children('article > a').click(function() {
+		var $content = $("<iframe>").attr({ src: $post.children('article > a').attr('href'), seamless: true });
+		openWindow($content, '<i class="fa fa-comments"></i>' + $post.children('article > header > h2 > a').text(), 300, 450);
 		return false;
 	});
 
