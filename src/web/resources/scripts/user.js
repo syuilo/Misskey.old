54b2084fc3cfdc55c14a1168d76d3d837ffea374
syuilo <syuilotan@yahoo.co.jp>
2015-02-16 15:55:22 +0900

    kiresou

--- src/web/resources/scripts/user.js
+++ src/web/resources/scripts/user.js
@@ -3,10 +3,10 @@ $(function() {
 		var $button = $(this);
 		$button.attr('disabled', true);
 
-		if ($('html').prop('data-isFollowing') == true) {
+		if ($('html').prop('data-is-following') == true) {
 			$.ajax('https://api.misskey.xyz/users/unfollow', {
 				type: 'delete',
-				data: JSON.stringify({ 'user_id': $('html').prop('data-userId') }),
+				data: JSON.stringify({ 'user_id': $('html').prop('data-user-id') }),
 				processData: false,
 				contentType: false,
 				dataType: 'json',
@@ -17,14 +17,14 @@ $(function() {
 				$button.attr('disabled', false);
 				$button.removeClass('following');
 				$button.addClass('notFollowing');
-				$('html').prop('data-isFollowing', false)
+				$('html').prop('data-is-following', false)
 			}).fail(function() {
 				$button.attr('disabled', false);
 			});
 		} else {
 			$.ajax('https://api.misskey.xyz/users/follow', {
 				type: 'post',
-				data: JSON.stringify({ 'user_id': $('html').prop('data-userId') }),
+				data: JSON.stringify({ 'user_id': $('html').prop('data-user-id') }),
 				processData: false,
 				contentType: false,
 				dataType: 'json',
@@ -35,7 +35,7 @@ $(function() {
 				$button.attr('disabled', false);
 				$button.removeClass('notFollowing');
 				$button.addClass('following');
-				$('html').prop('data-isFollowing', true)
+				$('html').prop('data-is-following', true)
 			}).fail(function() {
 				$button.attr('disabled', false);
 			});
