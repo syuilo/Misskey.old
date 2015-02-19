f3ac382e97e60d92daba56023476f074fb06d94b
syuilo <syuilotan@yahoo.co.jp>
2015-02-20 06:24:56 +0900

    Implement create and find method

--- src/models/post-favorite.ts
+++ src/models/post-favorite.ts
@@ -4,75 +4,86 @@ import db = require('../db');
 export = PostFavorite;
 
 class PostFavorite {
-	createdAt: string;
-	id: number;
-	postId: number;
-	userId: number;
+    createdAt: string;
+    id: number;
+    postId: number;
+    userId: number;
 
-	public constructor(postFavorite: any) {
-		this.createdAt = postFavorite.createdAt;
-		this.id = postFavorite.id;
-		this.postId = postFavorite.postId;
-		this.userId = postFavorite.user_id;
-	}
+    public constructor(postFavorite: any) {
+        this.createdAt = postFavorite.createdAt;
+        this.id = postFavorite.id;
+        this.postId = postFavorite.postId;
+        this.userId = postFavorite.user_id;
+    }
 
-	public static create(postId: number, userId: number, callback: (postFavorite: PostFavorite) => void): void {
-		db.query('insert into post_favorites (post_id, user_id) values (?, ?)',
-			[postId, userId],
-			(err: any, postFavorites: any[]) => callback(new PostFavorite(postFavorites[0])));
-	}
+    public static create(postId: number, userId: number, callback: (postFavorite: PostFavorite) => void): void {
+        db.query('insert into post_favorites (post_id, user_id) values (?, ?)',
+            [postId, userId],
+            (err: any, info: any) => {
+                if (err) console.log(err);
+                PostFavorite.find(info.insertId,(postFavorite: PostFavorite) => {
+                    callback(postFavorite);
+                });
+            });
+    }
 
-	public static isFavorited(postId: number, userId: number, callback: (favorite: boolean) => void): void {
-		db.query("select * from post_favorites where post_id = ? and user_id = ?",
-			[postId, userId],
-			(err: any, postFavorites: any[]) => callback(postFavorites.length !== 0));
-	}
+    public static find(id: number, callback: (postFavorite: PostFavorite) => void): void {
+        db.query("select * from post_favorites where id = ?",
+            [id],
+            (err: any, postFavorites: any[]) => callback(postFavorites[0] != null ? new PostFavorite(postFavorites[0]) : null));
+    }
 
-	public static findByPostId(postId: number, callback: (postFavorites: PostFavorite[]) => void): void {
-		db.query("select * from post_favorites where post_id = ?",
-			[postId],
-			(err: any, postFavorites: any[]) => callback(postFavorites.length != 0 ? postFavorites.map((postFavorite) => new PostFavorite(postFavorite)) : null));
-	}
+    public static isFavorited(postId: number, userId: number, callback: (favorite: boolean) => void): void {
+        db.query("select * from post_favorites where post_id = ? and user_id = ?",
+            [postId, userId],
+            (err: any, postFavorites: any[]) => callback(postFavorites.length !== 0));
+    }
 
-	public static findByUserId(userId: number, callback: (postFavorites: PostFavorite[]) => void): void {
-		db.query("select * from post_favorites where user_id = ?",
-			[userId],
-			(err: any, postFavorites: any[]) => callback(postFavorites.length != 0 ? postFavorites.map((postFavorite) => new PostFavorite(postFavorite)) : null));
-	}
+    public static findByPostId(postId: number, callback: (postFavorites: PostFavorite[]) => void): void {
+        db.query("select * from post_favorites where post_id = ?",
+            [postId],
+            (err: any, postFavorites: any[]) => callback(postFavorites.length != 0 ? postFavorites.map((postFavorite) => new PostFavorite(postFavorite)) : null));
+    }
 
-	public static getPostFavorites(postId: number, limit: number, offset: number, callback: (postFavorites: PostFavorite[]) => void): void {
-		var q: string, p: any;
-		if (limit === null) {
-			q = "select * from post_favorites where post_id = ? order by id desc";
-			p = [postId];
-		} else {
-			q = "select * from post_favorites where post_id = ? order by id desc limit ?, ?";
-			p = [postId, offset, limit];
-		}
-		db.query(q, p, (err: any, postFavorites: any[]) => callback(postFavorites.map((postFavorite) => new PostFavorite(postFavorite))));
-	}
+    public static findByUserId(userId: number, callback: (postFavorites: PostFavorite[]) => void): void {
+        db.query("select * from post_favorites where user_id = ?",
+            [userId],
+            (err: any, postFavorites: any[]) => callback(postFavorites.length != 0 ? postFavorites.map((postFavorite) => new PostFavorite(postFavorite)) : null));
+    }
 
-	public static getMyFavorites(userId: number, limit: number, offset: number, callback: (postFavorites: PostFavorite[]) => void): void {
-		var q: string, p: any;
-		if (limit === null) {
-			q = "select * from post_favorites where user_id = ? order by id desc";
-			p = [userId];
-		} else {
-			q = "select * from post_favorites where user_id = ? order by id desc limit ?, ?";
-			p = [userId, offset, limit];
-		}
-		db.query(q, p, (err: any, postFavorites: any[]) => callback(postFavorites.map((postFavorite) => new PostFavorite(postFavorite))));
-	}
+    public static getPostFavorites(postId: number, limit: number, offset: number, callback: (postFavorites: PostFavorite[]) => void): void {
+        var q: string, p: any;
+        if (limit === null) {
+            q = "select * from post_favorites where post_id = ? order by id desc";
+            p = [postId];
+        } else {
+            q = "select * from post_favorites where post_id = ? order by id desc limit ?, ?";
+            p = [postId, offset, limit];
+        }
+        db.query(q, p,(err: any, postFavorites: any[]) => callback(postFavorites.map((postFavorite) => new PostFavorite(postFavorite))));
+    }
 
-	public static getPostFavoritesCount(postId: number, callback: (favoritesCount: number) => void): void {
-		db.query("select count(*) as count from post_favorites where post_id = ?",
-			[postId],
-			(err: any, postFavorites: any[]) => callback(postFavorites[0].count));
-	}
+    public static getMyFavorites(userId: number, limit: number, offset: number, callback: (postFavorites: PostFavorite[]) => void): void {
+        var q: string, p: any;
+        if (limit === null) {
+            q = "select * from post_favorites where user_id = ? order by id desc";
+            p = [userId];
+        } else {
+            q = "select * from post_favorites where user_id = ? order by id desc limit ?, ?";
+            p = [userId, offset, limit];
+        }
+        db.query(q, p,(err: any, postFavorites: any[]) => callback(postFavorites.map((postFavorite) => new PostFavorite(postFavorite))));
+    }
+
+    public static getPostFavoritesCount(postId: number, callback: (favoritesCount: number) => void): void {
+        db.query("select count(*) as count from post_favorites where post_id = ?",
+            [postId],
+            (err: any, postFavorites: any[]) => callback(postFavorites[0].count));
+    }
 
     public destroy(callback?: () => void): void {
-		db.query('delete from post_favorites where post_id = ? and user_id = ?"',
-			[this.postId, this.userId],
-			callback);
-	}
+        db.query('delete from post_favorites where post_id = ? and user_id = ?"',
+            [this.postId, this.userId],
+            callback);
+    }
 }
