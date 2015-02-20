eca572842e039d57a9338570dc07872ed9d3ace7
syuilo <syuilotan@yahoo.co.jp>
2015-02-20 18:03:51 +0900

    Use void 0

--- src/api/rest/account/update.ts
+++ src/api/rest/account/update.ts
@@ -11,14 +11,14 @@ var authorize = require('../../auth');
 var accountUpdate = (req: any, res: APIResponse) => {
 	authorize(req, res,(user: User, app: Application) => {
 		var params = req.body;
-		user.name = params.name != null ? params.name : user.name;
-		user.comment = params.comment != null ? params.comment : user.comment;
-		user.badge = params.badge != null ? params.badge : user.badge;
-		user.url = params.url != null ? params.url : user.url;
-		user.location = params.location != null ? params.location : user.location;
-		user.bio = params.bio != null ? params.bio : user.bio;
-		user.tag = params.tag != null ? params.tag : user.tag;
-		user.color = params.color != null ? params.color.match(/#[a-fA-F0-9]+/) ? params.color : user.color : user.color;
+		user.name = params.name !== void 0 ? params.name : user.name;
+		user.comment = params.comment !== void 0 ? params.comment : user.comment;
+		user.badge = params.badge !== void 0 ? params.badge : user.badge;
+		user.url = params.url !== void 0 ? params.url : user.url;
+		user.location = params.location !== void 0 ? params.location : user.location;
+		user.bio = params.bio !== void 0 ? params.bio : user.bio;
+		user.tag = params.tag !== void 0 ? params.tag : user.tag;
+		user.color = params.color !== void 0 ? params.color.match(/#[a-fA-F0-9]+/) ? params.color : user.color : user.color;
 		user.update(() => {
 			res.apiRender(user.filt());
 		});
