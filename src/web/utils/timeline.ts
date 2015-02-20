096b32f0e1fc2f2cf85b4105e2665a8e7a3725c4
syuilo <syuilotan@yahoo.co.jp>
2015-02-20 18:31:05 +0900

    Fix #13

--- src/web/utils/timeline.ts
+++ src/web/utils/timeline.ts
@@ -37,8 +37,8 @@ class Timeline {
 
 	public static selialyzeTimelineOnject(posts: Post[], req: any, callback: (posts: any[]) => void): void {
 		async.map(posts,(post: any, mapNext: any) => {
-			if (post.repostFromPostId != null) {
-				Post.find(post.repostFromPostId,(repostFromPost: any) => {
+			if (post.repostFromPostId != null && post.repostFromPostId != 0) {
+				Post.find(post.repostFromPostId,(repostFromPost: Post) => {
 					var _repostFromPost: any = repostFromPost;
 					_repostFromPost.isRepostToPost = true;
 					User.find(post.userId,(repostedByUser: User) => {
