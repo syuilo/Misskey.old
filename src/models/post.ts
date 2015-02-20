5b394b2345c9ce81a79b97b01a05c30069f87baf
syuilo <syuilotan@yahoo.co.jp>
2015-02-20 14:16:02 +0900

    Tuning query

--- src/models/post.ts
+++ src/models/post.ts
@@ -66,9 +66,9 @@ class Post {
 	}
 
 	public static isReposted(postId: number, userId: number, callback: (favorite: boolean) => void): void {
-		db.query("select id from posts where repost_from_post_id = ? and user_id = ?",
+		db.query("select exists (select * from posts where repost_from_post_id = ? and user_id = ?)",
 			[postId, userId],
-			(err: any, postReposts: any[]) => callback(postReposts.length !== 0));
+			(err: any, postReposts: any[]) => callback(postReposts.length > 0));
 	}
 
 	public static findByUserId(userId: number, limit: number, sinceId: number, maxId: number, callback: (posts: Post[]) => void): void {
