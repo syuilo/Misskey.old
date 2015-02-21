afefec84d51f456e6fc886b15667d6c4e8891f23
syuilo <syuilotan@yahoo.co.jp>
2015-02-21 15:53:34 +0900

    Repostできない問題を修正

--- src/api/rest/post/repost.ts
+++ src/api/rest/post/repost.ts
@@ -47,7 +47,7 @@ function repostStep(req: any, res: APIResponse, app: Application, user: User, ta
 		}
 
 		User.find(targetPost.userId,(targetPostUser: User) => {
-			Post.create(app.id, null, null, null, 'RP @' + targetPostUser.screenName + ' ' + targetPost.text, user.id, targetPost.id,(post: Post) => {
+			Post.create(app.id, null, null, 'RP @' + targetPostUser.screenName + ' ' + targetPost.text, user.id, targetPost.id,(post: Post) => {
 				targetPost.repostsCount++;
 				targetPost.update(() => { });
 				Post.buildResponseObject(targetPost,(targetPostObj: any) => {
