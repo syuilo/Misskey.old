819ad7c63ac84b620642184ab9d6afdf89788d9d
syuilo <syuilotan@yahoo.co.jp>
2015-02-28 17:44:06 +0900

    Implement alive signal

--- src/api/socketio/talk.ts
+++ src/api/socketio/talk.ts
@@ -41,6 +41,10 @@ var sarver = (io: any, sessionStore: any): void => {
 					});
 				});
 
+				socket.on('alive',(req: any) => {
+					publisher.publish('misskey:talkStream:' + socket.otherpartyId + '-' + uid, 'alive');
+				});
+
 				socket.on('type',(req: any) => {
 					publisher.publish('misskey:talkStream:' + socket.otherpartyId + '-' + uid, JSON.stringify({
 						type: 'type',
