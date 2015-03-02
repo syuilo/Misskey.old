81f38669284a90090fe35e63d559b11b6f9aa30a
syuilo <syuilotan@yahoo.co.jp>
2015-03-03 05:12:37 +0900

    Fix file encoding

--- src/web/controllers/dev.ts
+++ src/web/controllers/dev.ts
@@ -10,7 +10,7 @@ export = render;
 var render = (req: any, res: any): void => {
 	async.series([
 		(callback: any) => {
-            Application.findByUserId(req.me.id,(apps: Application[]) => {
+			Application.findByUserId(req.me.id,(apps: Application[]) => {
 				callback(null, apps);
 			});
 		}],
