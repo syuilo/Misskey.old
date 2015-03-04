53c96aec9200d6826ac0e09854228cc01dd5188e
syuilo <syuilotan@yahoo.co.jp>
2015-03-04 16:11:55 +0900

    req.rottUser を渡すように修正

--- src/web/controllers/post.ts
+++ src/web/controllers/post.ts
@@ -10,27 +10,23 @@ export = render;
 var render = (req: any, res: any): void => {
 	async.series([
 		(callback: any) => {
-			User.find(req.rootPost.userId, (user: User) => {
-				callback(null, user);
-			});
-		},
-		(callback: any) => {
-			Post.getBeforeTalk(req.rootPost.id, (posts: Post[]) => {
-				async.map(posts, (post: any, next: any) => {
-					User.find(post.userId, (user: User) => {
+			Post.getBeforeTalk(req.rootPost.id,(posts: Post[]) => {
+				async.map(posts,(post: any, next: any) => {
+					User.find(post.userId,(user: User) => {
 						post.user = user;
 						next(null, user);
 					});
-				}, (err: any, results: any[]) => {
-					callback(null, results);
-				});
+				},(err: any, results: any[]) => {
+						callback(null, results);
+					});
 			});
 		}
-	], (err: any, results: any) => {
-		res.display(req, res, 'post', {
-			post: req.rootPost,
-			postUser: results[0],
-			beforeTalks: results[1]
+	],(err: any, results: any) => {
+			var post: any = req.rootPost;
+			post.user = req.rootUser;
+			res.display(req, res, 'post', {
+				post: post,
+				beforeTalks: results[0]
+			});
 		});
-	});
 };
