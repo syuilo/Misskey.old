7789611c022ba9c94a446cae71c55d53b02a0793
marihachi <marihachi0620@gmail.com>
2015-03-09 00:04:29 +0900

    Update Developer Center

--- src/web/resources/scripts/dev.js
+++ src/web/resources/scripts/dev.js
@@ -14,47 +14,53 @@ $(function() {
 	});
 
 	$('#myApp ul li').click(function () {
-		var idName = $(this).attr("id");
-		$.ajax({
-			url: 'https://misskey.xyz/dev/myapp?q=' + idName,
-			type: 'GET',
-			dataType: 'html',
-		})
-		.done(function(data) {
-			$("main").html($(data).html());
-		})
-		.fail(function(data) {
-			//失敗時
-		});
+		if($(this).children().prop("nodeType") != 1) {
+			var idName = $(this).attr("id");
+			$.ajax({
+				url: 'https://misskey.xyz/dev/myapp?q=' + idName,
+				type: 'GET',
+				dataType: 'html',
+			})
+			.done(function(data) {
+				$("main").html($(data).html());
+			})
+			.fail(function(data) {
+				//失敗時
+			});
+		}
 	});
 
 	$('#restApi ul li').click(function () {
-		var idName = $(this).attr("id");
-		$.ajax({
-			url: 'https://misskey.xyz/dev/reference?q=' + idName,
-			type: 'GET',
-			dataType: 'html',
-		})
-		.done(function(data) {
-			$("main").html($(data).children("main").html());
-		})
-		.fail(function(data) {
-			//失敗時
-		});
+		if($(this).children().prop("nodeType") != 1) {
+			var idName = $(this).attr("id");
+			$.ajax({
+				url: 'https://misskey.xyz/dev/reference?q=' + idName,
+				type: 'GET',
+				dataType: 'html',
+			})
+			.done(function(data) {
+				$("main").html($(data).children("main").html());
+			})
+			.fail(function(data) {
+				//失敗時
+			});
+		}
 	});
 
 	$('#userTheme ul li').click(function () {
-		var idName = $(this).attr("id");
-		$.ajax({
-			url: 'https://misskey.xyz/dev/usertheme?q=' + idName,
-			type: 'GET',
-			dataType: 'html',
-		})
-		.done(function(data) {
-			$("main").html($(data).html());
-		})
-		.fail(function(data) {
-			//失敗時
-		});
+		if($(this).children().prop("nodeType") != 1) {
+			var idName = $(this).attr("id");
+			$.ajax({
+				url: 'https://misskey.xyz/dev/usertheme?q=' + idName,
+				type: 'GET',
+				dataType: 'html',
+			})
+			.done(function(data) {
+				$("main").html($(data).html());
+			})
+			.fail(function(data) {
+				//失敗時
+			});
+		}
 	});
 });
