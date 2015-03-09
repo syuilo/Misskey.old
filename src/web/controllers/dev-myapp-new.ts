de3a119465f5513a875cdc4d2fec50253044c79b
marihachi <marihachi0620@gmail.com>
2015-03-10 03:32:39 +0900

    Added controller

---
+++ src/web/controllers/dev-myapp-new.ts
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../../typings/bundle.d.ts" />
+
+import async = require('async');
+import Application = require('../../models/application');
+import conf = require('../../config');
+
+export = render;
+
+var render = (req: any, res: any): void => {
+	res.display(req, res, 'dev-myapp-new', {});
+};
