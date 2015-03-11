b893b67c1ddc301858f480d2e584f6a2acd5a848
syuilo <syuilotan@yahoo.co.jp>
2015-03-11 20:58:40 +0900

    Cache maintenance message

--- src/web/maintenance.ts
+++ src/web/maintenance.ts
@@ -1,5 +1,6 @@
 /// <reference path="../../typings/bundle.d.ts" />
 
+import jade = require('jade');
 import express = require('express');
 import minify = require('express-minify');
 import compress = require('compression');
@@ -8,17 +9,18 @@ import config = require('../config');
 
 var webServer: any = express();
 webServer.disable('x-powered-by');
-webServer.set('view engine', 'jade');
-webServer.set('views', __dirname + '/views');
 
 /* Compressing settings */
 webServer.use(compress());
 webServer.use(minify());
 
+/* Precompile */
+var message = jade.renderFile(__dirname + '/views/maintenance.jade');
+
 /* General rooting */
 webServer.all('*',(req: express.Request, res: express.Response, next: () => void) => {
 	res.status(503);
-	res.render('maintenance');
+	res.send(message);
 });
 
 webServer.listen(config.port.web);
