efddc17e27a23a013c03cf161d1049063992f0ce
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-03-14 22:26:52 +0900

    Improve readability

--- src/api/rest/application/create.ts
+++ src/api/rest/application/create.ts
@@ -14,25 +14,12 @@ function createApplication(req: any, res: APIResponse): void {
 		var developerName = typeof req.body.developer_name !== 'undefined' ? req.body.developer_name : null;
 		var developerWebsite = typeof req.body.developer_website !== 'undefined' ? req.body.developer_website : null;
 		
-		if (app.name !== 'Web') {
-			res.apiError(403, 'Your application has no permission');
-		} else if (name === null || name === '') {
-			res.apiError(400, 'name cannot be empty :(');
-		} else if (name.length > 32) {
-			res.apiError(400, 'name cannot be more than 32 charactors');
-		} else if (callbackUrl === null || callbackUrl === '') {
-			res.apiError(400, 'callback_url cannot be empty :(');
-		} else if (description === null) {
-			res.apiError(400, 'description cannot be empty :(');
-		} else if (description.length < 10 || 400 < description.length) {
-			res.apiError(400, 'description cannot be less than 10 charactors and more than 400 charactors');
-		} else if (developerName === null || developerName === '') {
-			res.apiError(400, 'developer_name cannot be empty :(');
-		} else if (developerWebsite === null || developerWebsite === '') {
-			res.apiError(400, 'developer_website cannot be empty :(');
+		var error = validateArguments();
+		if (error !== null) {
+			res.apiError(error[0], error[1]);
 		} else if (!user.isPremium) {
-			Application.findByUserId(user.id, (apps: Application[]) => {
-				if (1 <= apps.length) {
+			hasAppOneOrMore(oneOrMore => {
+				if (oneOrMore) {
 					res.apiError(400, 'cannot create application at two or more. need PlusAccount to do so.');
 				} else {
 					create();
@@ -40,6 +27,33 @@ function createApplication(req: any, res: APIResponse): void {
 			});
 		} else {
 			create();
+		}
+		
+		function validateArguments(): [number, string] {
+			switch(true) {
+				case app.name !== 'Web':
+					return [403, 'Your application has no permission'];
+				case isNullOrEmpty(name):
+					return [400, 'name cannot be empty :('];
+				case name.length > 32:
+					return [400, 'name cannot be more than 32 charactors'];
+				case isNullOrEmpty(callbackUrl):
+					return [400, 'callback_url cannot be empty :('];
+				case description === null:
+					return [400, 'description cannot be empty :('];
+				case description.length < 10 || 400 < description.length:
+					return [400, 'description cannot be less than 10 charactors and more than 400 charactors'];
+				case isNullOrEmpty(developerName):
+					return [400, 'developer_name cannot be empty :('];
+				case isNullOrEmpty(developerWebsite):
+					return [400, 'developer_website cannot be empty :('];
+				default:
+					return null;
+			}
+		}
+		
+		function hasAppOneOrMore(callback: (oneOrMore: boolean) => void): void {
+			Application.findByUserId(user.id, (apps: Application[]) => callback(1 <= apps.length));
 		}
 		
 		function create() {
@@ -52,6 +66,10 @@ function createApplication(req: any, res: APIResponse): void {
 			});
 		}
 	});
+}
+
+function isNullOrEmpty(obj: string): boolean {
+	return obj === null || obj === '';
 }
 
 export = createApplication;
