bd876e3251a143835fcb3502a87bdf5f2ccb235d
syuilo <syuilotan@yahoo.co.jp>
2015-03-15 03:21:01 +0900

    Fix indent

--- src/models/mongo/user.ts
+++ src/models/mongo/user.ts
@@ -5,26 +5,26 @@ import mongoose = require('mongoose');
 var db = mongoose.connect('mongodb://localhost/Misskey');
 
 var userSchema = new mongoose.Schema({
-	bio:				{ type: String },
-	birthday:			{ type: String },
-	color:				{ type: String },
-	comment:			{ type: String },
-	createdAt:			{ type: Date, default: Date.now, required: true },
-	emailaddress:		{ type: Number },
-	exp:				{ type: String },
-	firstName:			{ type: String },
-	gender:				{ type: String },
-	isPlused:			{ type: Boolean },
-	isSuspended:		{ type: Boolean }
-	lang:				{ type: String },
-	lastName:			{ type: String },
-	location:			{ type: String },
-	lv:					{ type: Number },
-	name:				{ type: String },
-	password:			{ type: String },
-	screenName:			{ type: String, required: true },
-	url:				{ type: String },
-	usingWebThemeId:	{ type: Number },
+	bio: { type: String },
+	birthday: { type: String },
+	color: { type: String },
+	comment: { type: String },
+	createdAt: { type: Date, default: Date.now, required: true },
+	emailaddress: { type: Number },
+	exp: { type: String },
+	firstName: { type: String },
+	gender: { type: String },
+	isPlused: { type: Boolean },
+	isSuspended: { type: Boolean }
+	lang: { type: String },
+	lastName: { type: String },
+	location: { type: String },
+	lv: { type: Number },
+	name: { type: String },
+	password: { type: String },
+	screenName: { type: String, required: true },
+	url: { type: String },
+	usingWebThemeId: { type: Number },
 });
 
 export = db.model('User', userSchema);
