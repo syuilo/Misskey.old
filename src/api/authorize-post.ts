581c8db4bf46e8ed0e45c358992c73b428f6a512
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-03-14 20:58:48 +0900

    Improve readability

--- src/api/authorize-post.ts
+++ src/api/authorize-post.ts
@@ -7,72 +7,71 @@ import Application = require('../models/application');
 import User = require('../models/user');
 import doLogin = require('../web/utils/login');
 
-var AuthorizePost = (req: any, res: any, server: any) => {
+function AuthorizePost(req: any, res: any, server: any): void {
 	var login = (req.session != null && req.session.userId != null);
-	if (req.query.request_token != null) {
-		var requestToken = req.query.request_token;
-		SauthRequestToken.find(requestToken,(requestTokenInstance: SauthRequestToken) => {
-			if (requestTokenInstance != null) {
-				if (requestTokenInstance.isInvalid) {
-					return;
-				}
-				Application.find(requestTokenInstance.appId,(app: Application) => {
-					if (req.body.screen_name != null && req.body.password != null) {
-						doLogin(server, req, req.body.screen_name, req.body.password,(user: User, webAccessToken: AccessToken) => {
+	var requestToken = typeof req.query.request_token !== 'undefined' ? req.query.request_token : null;
+	var screenName = typeof req.body.screen_name !== 'undefined' ? req.body.screen_name : null;
+	var password = typeof req.body.password !== 'undefined' ? req.body.password : null;
+	if (requestToken !== null) {
+		SauthRequestToken.find(requestToken, (requestTokenInstance: SauthRequestToken) => {
+			if (requestTokenInstance != null && !requestTokenInstance.isInvalid) {
+				Application.find(requestTokenInstance.appId, (app: Application) => {
+					if (screenName !== null && password !== null) {
+						doLogin(server, screenName, password, (user: User, webAccessToken: AccessToken) => {
 							validate(requestTokenInstance, user, app);
-						},() => {
-								res.render('../web/views/authorize-confirm', {
-									login: false,
-									app: app,
-									loginFailed: true
-								});
-							});
+						}, renderConfirmation);
+					} else if (login) {
+						User.find(req.session.userId, (user: User) => {
+							validate(requestTokenInstance, user, app);
+						});
 					} else {
-						if (login) {
-							User.find(req.session.userId,(user: User) => {
-								validate(requestTokenInstance, user, app);
-							});
-						} else {
-							res.render('../web/views/authorize-confirm', {
-								login: false,
-								app: app,
-								loginFailed: false
-							});
-						}
+						renderConfirmation();
 					}
-				});
-			} else {
-
+				})
 			}
 		});
-	} else {
-
 	}
-
-	var validate = (requestTokenInstance: SauthRequestToken, user: User, app: Application) => {
+	
+	function validate(requestTokenInstance: SauthRequestToken, user: User, app: Application): void {
 		if (req.body.cancel != null) {
 			requestTokenInstance.isInvalid = true;
 			requestTokenInstance.update();
-			res.render('../web/views/authorize-cancel', {
-				login: true,
-				app: app,
-				me: user
-			});
+			renderCancel();
 		} else {
-			SauthPinCode.create(app.id, user.id,(pincode: SauthPinCode) => {
-				if (app.callbackUrl == '') {
-					res.render('../web/views/authorize-success', {
-						login: true,
-						me: user,
-						app: app,
-						code: pincode.code
-					});
+			SauthPinCode.create(app.id, user.id, (pincode: SauthPinCode) => {
+				if (app.callbackUrl === '') {
+					renderSuccess();
 				} else {
 					res.redirect(app.callbackUrl + '?pincode=' + pincode.code);
 				}
 			});
 		}
-	};
+	}
+	
+	function renderConfirmation(): void {
+		res.render('../web/views/authorize-confirm', {
+			login: false,
+			app: app,
+			loginFailed: false
+		});
+	}
+
+	function renderCancel(): void {
+		res.render('../web/views/authorize-cancel', {
+			login: true,
+			app: app,
+			me: user
+		});
+	}
+	
+	function renderSuccess(): void {
+		res.render('../web/views/authorize-success', {
+			login: true,
+			me: user,
+			app: app,
+			code: pincode.code
+		});
+	}
 }
 
-module.exports = AuthorizePost;
+export = AuthorizePost;
