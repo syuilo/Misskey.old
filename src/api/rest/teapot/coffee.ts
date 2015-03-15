58c787a07368020f520fe47e16e9eb53609b0bf0
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-03-16 04:41:08 +0900

    coffee.ts to coffee.ls

--- src/api/rest/teapot/coffee.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../../../typings/bundle.d.ts" />
-
-import APIResponse = require('../../api-response');
-
-var teapotCoffee = (req: any, res: APIResponse) => {
-	res.apiError(418, "I'm a teapot.");
-}
-
-module.exports = teapotCoffee;
