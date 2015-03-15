e805acb1e97f8f798895b90df294f0b588dc3656
Aya Morisawa <AyaMorisawa4869@gmail.com>
2015-03-15 22:14:37 +0900

    Improve readability

--- src/web/routes/index.ts
+++ src/web/routes/index.ts
@@ -1,137 +1,67 @@
-/// <reference path="../../../typings/bundle.d.ts" />
-
-import fs = require('fs');
-import express = require('express');
-import AccessToken = require('../../models/access-token');
-import User = require('../../models/user');
-import Post = require('../../models/post');
-import doLogin = require('../utils/login');
-import config = require('../../config');
-import imageRouter = require('./image');
-
-export = router;
-
-var router = (app: express.Express): void => {
-	/* Image */
-	imageRouter(app);
+require! {
+	fs
+	express
+	'../../models/access-token': AccessToken
+	'../../models/user': User
+	'../../models/post': Post
+	'../utils/login': do-login
+	'../../config': config
+	'./image': image-router
+}
+module.exports = (app) ->
+	image-router app
 	
-	app.param('userSn',(req: any, res: any, next: () => void, sn: string) => {
-		User.findByScreenName(sn,(user: User) => {
-			if (user != null) {
-				req.rootUser = user;
-				req.data.rootUser = user;
-				next();
-			} else {
-				res.status(404);
-				res.display(req, res, 'user-notFound', {});
-			}
-		});
-	});
-
-	app.param('postId', (req: any, res: any, next: () => void , postId: number) => {
-		Post.find(postId, (post: Post) => {
-			if (post != null) {
-				req.rootPost = post;
-				req.data.rootPost = post;
-				next();
-			} else {
-				req.status(404);
-				res.display(req, res, 'post-notFound', {});
-			}
-		});
-	});
-
-	app.get('/',(req: any, res: any, next: () => void) => {
-		if (req.login) {
-			require('../controllers/home')(req, res);
-		} else {
-			res.display(req, res, 'entrance', {});
-		}
-	});
-
-	app.get('/new',(req: any, res: any, next: () => void) => {
-		require('../controllers/new')(req, res);
-	});
-
-	app.get('/i/mention',(req: any, res: any, next: () => void) => {
-		require('../controllers/i-mention')(req, res);
-	});
-	app.get('/i/mentions',(req: any, res: any, next: () => void) => {
-		require('../controllers/i-mention')(req, res);
-	});
-
-	app.get('/i/talk',(req: any, res: any, next: () => void) => {
-		require('../controllers/i-talks')(req, res);
-	});
-	app.get('/i/talks',(req: any, res: any, next: () => void) => {
-		require('../controllers/i-talks')(req, res);
-	});
-
-	app.get('/i/setting',(req: any, res: any, next: () => void) => {
-		require('../controllers/i-setting')(req, res);
-	});
-	app.get('/i/settings',(req: any, res: any, next: () => void) => {
-		require('../controllers/i-setting')(req, res);
-	});
-
-	app.get('/config',(req: any, res: any, next: () => void) => {
-		res.set('Content-Type', 'application/javascript');
-		res.send('var conf = ' + JSON.stringify(config.publicConfig) + ';');
-	});
-
-	app.get('/dev',(req: any, res: any, next: () => void) => {
-		require('../controllers/dev')(req, res);
-	});
-	app.get('/dev/reference', (req: any, res: any, next: () => void) => {
-		require('../controllers/dev-reference')(req, res);
-	});
-	app.get('/dev/myapp', (req: any, res: any, next: () => void) => {
-		require('../controllers/dev-myapp')(req, res);
-	});
-	app.get('/dev/myapp/new', (req: any, res: any, next: () => void) => {
-		require('../controllers/dev-myapp-new')(req, res);
-	});
-	app.get('/dev/usertheme', (req: any, res: any, next: () => void) => {
-		require('../controllers/dev-usertheme')(req, res);
-	});
-	app.get('/dev/usertheme/new', (req: any, res: any, next: () => void) => {
-		require('../controllers/dev-usertheme-new')(req, res);
-	});
-
-	/* Actions */
-	app.get('/login',(req: any, res: any) => {
-		res.display(req, res, 'login', {});
-	});
-
-	app.post('/login',(req: any, res: any) => {
-		doLogin(req, req.body.screen_name, req.body.password,(user: User, webAccessToken: AccessToken) => {
-			res.sendStatus(200);
-		},() => res.sendStatus(400));
-	});
-
-	app.get('/logout',(req: any, res: any) => {
-		req.session.destroy((err: any) => {
-			res.redirect('/');
-		});
-	});
-
-	app.get('/:userSn',(req: any, res: any, next: () => void) => {
-		require('../controllers/user')(req, res, 'home');
-	});
-	app.get('/:userSn/followings',(req: any, res: any, next: () => void) => {
-		require('../controllers/user')(req, res, 'followings');
-	});
-	app.get('/:userSn/followers',(req: any, res: any, next: () => void) => {
-		require('../controllers/user')(req, res, 'followers');
-	});
-
-	app.get('/:userSn/talk', require('../controllers/user-talk'));
-
-	app.get('/:userSn/:postId(\\d+)', (req: any, res: any, next: () => void) => {
-		require('../controllers/post')(req, res);
-	});
-
-	app.get('/:userSn/post/:postId(\\d+)',(req: any, res: any, next: () => void) => {
-		require('../controllers/post')(req, res);
-	});
-};
+	app
+		..param
+			.. 'userSn' (req, res, next, sn) ->
+				User.find-by-screen-name sn, (user) ->
+					if user != null
+						req.root-user = req.data.root-user = user
+						next!
+					else
+						res.status 404
+						
+			.. 'postId' (req, res, next, post-id) ->
+				Post.find post-id, (post) ->
+					if post != null
+						req.root-post = req.data.root-post = post
+						next!
+					else
+						res.status 404
+						res.display req, res, 'post-notFound', {}
+		..get
+			.. '/' (req, res, next) ->
+				if req.login
+					then (require '../controllers/home') req, res
+					else res.display req, res, 'entrance', {}
+			.. '/new' (req, res, next) -> (require '../controllers/new') req, res
+			.. '/i/mention' (req, res, next) -> (require '../controllers/i-mention') req, res
+			.. '/i/mentions' (req, res, next) -> (require '../controllers/i-mention') req, res
+			.. '/i/talk' (req, res, next) -> (require '../controllers/i-talks') req, res
+			.. '/i/talks' (req, res, next) -> (require '../controllers/i-talks') req, res
+			.. '/i/setting' (req, res, next) -> (require '../controllers/i-setting') req, res
+			.. '/i/settings' (req, res, next) -> (require '../controllers/i-setting') req, res
+			.. '/config' (req, res, next) ->
+				res.set 'Content-Type' 'application/javascript'
+				res.send 'var conf = ' + (JSON.stringify config.public-config) + ';'
+			.. '/dev' (req, res, next) -> (require '../controllers/dev') req, res
+			.. '/dev/reference' (req, res, next) -> (require '../controllers/dev-reference') req, res
+			.. '/dev/myapp' (req, res, next) -> (require '../controllers/dev-myapp') req, res
+			.. '/dev/myapp/new' (req, res, next) -> (require '../controllers/dev-myapp-new') req, res
+			.. '/dev/usertheme' (req, res, next) -> (require '../controllers/dev-usertheme') req, res
+			.. '/dev/usertheme/new' (req, res, next) -> (require '../controllers/dev-usertheme-new') req, res
+			.. '/login' (req, res) -> res.display req, res, 'login', {}
+		..post '.login' (req, res) ->
+			doLogin req, req.body.screen_name, req.body.password, (user, webAccessToken) ->
+				res.sendStatus200
+			, -> res.sendStatus 400
+		..get
+			.. '/logout' (reqy, res) ->
+				req.session.destroy (err) -> res.redirect '/'
+			.. '/:userSn' (req, res, next) -> (require '../controllers/user') req, res, \home
+			.. '/:userSn/followings' (req, res, next) -> (require '../controllers/user') req, res, \followings
+			.. '/:userSn/followers' (req, res, next) -> (require '../controllers/user') req, res, \followers
+			..  (req, res, next) -> (require ) req, res, 
+			.. '/:userSn/talk' require '../controllers/user-talk'
+			.. '/:userSn/:postId(\\d+)' (req, res, next) -> (require '../controllers/post') req, res
+			.. '/:userSn/post/:postId(\\d+)' (req, res, next) -> (require '../controllers/post') req, res
